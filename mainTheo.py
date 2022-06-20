32a9e7d4d9bf99ee10bf872fab711a0787256b87
Sooniii <theodulejanchen@hotmail.fr>
2022-06-20 10:44:12 +0200

    v1.0.0 Fin script

--- mainTheo.py
+++ mainTheo.py
@@ -1,1 +1,6 @@
-print("Hello World")
+
+while (True):
+    year = int(input("Entre l'année :"))
+    if(year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+        print("L'année est bisextile")
+    else:print("L'année n'est pas bisextile")
